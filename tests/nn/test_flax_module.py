901348b6f11761712ea748495acaf8040ee80393
Cristian Garcia <cgarcia.e88@gmail.com>
2021-09-07 13:05:51 -0500

    improve FlaxModule

--- tests/nn/test_flax_module.py
+++ tests/nn/test_flax_module.py
@@ -1,4 +1,5 @@
 import flax
+import jax
 import numpy as np
 
 import treex as tx
@@ -24,3 +25,121 @@ class TestFlaxModule:
         ).init(42)
 
         y = treex_module(x, training=True)
+
+    def test_pretrained_flax_module(self):
+        class SomeModule(flax.linen.Module):
+            @flax.linen.compact
+            def __call__(self, x, training, rng):
+                x = flax.linen.Dense(16)(x)
+                x = flax.linen.BatchNorm()(x, use_running_average=not training)
+                x = flax.linen.Dropout(0.5)(x, deterministic=not training, rng=rng)
+                x = flax.linen.Conv(16, [3])(x)
+
+                return x
+
+        x = np.ones((2, 5, 8), dtype=np.float32)
+        training = True
+        rng = jax.random.PRNGKey(42)
+
+        flax_module = SomeModule()
+        params_key, dropout_key = jax.random.split(jax.random.PRNGKey(0))
+        variables = flax_module.init(
+            {"params": params_key, "dropout": dropout_key},
+            x,
+            training,
+            rng,
+        )
+
+        treex_module = tx.FlaxModule(
+            SomeModule(),
+            sample_inputs=tx.Inputs(x, training=True),
+            variables=variables,
+        ).init(42)
+
+        y_treex = treex_module(x, training, rng)
+        y_treex = treex_module(x, training, rng)
+
+        y_flax, updates = flax_module.apply(
+            variables,
+            x,
+            training,
+            rng,
+            mutable=["batch_stats"],
+        )
+        variables = variables.copy(updates)
+        y_flax, updates = flax_module.apply(
+            variables,
+            x,
+            training,
+            rng,
+            mutable=["batch_stats"],
+        )
+        variables = variables.copy(updates)
+
+        assert np.allclose(y_treex, y_flax)
+
+        assert all(
+            np.allclose(a, b)
+            for a, b in zip(
+                jax.tree_leaves(variables["params"]),
+                jax.tree_leaves(treex_module.filter(tx.Parameter)),
+            )
+        )
+
+    def test_pretrained_flax_module_no_rng(self):
+        class SomeModule(flax.linen.Module):
+            @flax.linen.compact
+            def __call__(self, x, training):
+                x = flax.linen.Dense(16)(x)
+                x = flax.linen.BatchNorm()(x, use_running_average=not training)
+                x = flax.linen.Dropout(0.5)(x, deterministic=not training)
+                x = flax.linen.Conv(16, [3])(x)
+
+                return x
+
+        x = np.ones((2, 5, 8), dtype=np.float32)
+        training = True
+
+        flax_module = SomeModule()
+        params_key, dropout_key = jax.random.split(jax.random.PRNGKey(0))
+        variables = flax_module.init(
+            {"params": params_key, "dropout": dropout_key},
+            x,
+            training,
+        )
+
+        treex_module = tx.FlaxModule(
+            flax_module,
+            sample_inputs=tx.Inputs(x, training=True),
+            variables=variables,
+        ).init(42)
+        flax_key = treex_module.rng_seq.key
+
+        # step 1
+        next_key, flax_key = jax.random.split(flax_key)
+        y_flax, updates = flax_module.apply(
+            variables,
+            x,
+            training,
+            rngs={"dropout": next_key},
+            mutable=["batch_stats"],
+        )
+        variables = variables.copy(updates)
+        y_treex = treex_module(x, training)
+
+        assert np.allclose(flax_key, treex_module.rng_seq.key)
+
+        # step 2
+        next_key, flax_key = jax.random.split(flax_key)
+        y_flax, updates = flax_module.apply(
+            variables,
+            x,
+            training,
+            rngs={"dropout": next_key},
+            mutable=["batch_stats"],
+        )
+        variables = variables.copy(updates)
+        y_treex = treex_module(x, training)
+        assert np.allclose(flax_key, treex_module.rng_seq.key)
+
+        assert np.allclose(y_treex, y_flax)
--- treex/nn/flax_module.py
+++ treex/nn/flax_module.py
@@ -29,30 +29,36 @@ class FlaxModule(Module):
     def __init__(
         self,
         module: flax.linen.Module,
-        sample_inputs: types.Inputs,
-        mutable: tp.Sequence[str] = ("batch_stats", "cache"),
-        rngs: tp.Sequence[str] = (
-            "params",
-            "dropout",
-        ),
+        sample_inputs: tp.Optional[types.Inputs] = None,
+        mutable: tp.Sequence[str] = ("params", "batch_stats", "cache"),
+        rngs: tp.Sequence[str] = ("dropout",),
         variables: tp.Optional[FrozenDict] = None,
     ) -> None:
         super().__init__()
         self.module = types.Hashable(module)
         self.mutable = tuple(mutable)
         self.rngs = tuple(rngs)
-        self.sample_inputs = sample_inputs
-        self._process_variables(variables)
+        self.sample_inputs = (
+            sample_inputs if sample_inputs is not None else types.Inputs()
+        )
         self.rng_seq = RngSeq()
+        self.params = None
+        self.batch_stats = None
+        self.cache = None
+        self.variables = None
+
+        if variables is not None:
+            self._update_variables(variables)
 
     def module_init(self, key: jnp.ndarray) -> None:
-        assert self.sample_inputs is not None
-        rngs = self._get_rngs()
-        variables = self.module.value.init(
-            rngs, *self.sample_inputs.args, **self.sample_inputs.kwargs
-        )
-        self._process_variables(variables)
-        self.sample_inputs = None
+        if self.variables is None:
+            assert self.sample_inputs is not None
+            rngs = self._get_rngs(self.rngs + self.mutable)
+            variables = self.module.value.init(
+                rngs, *self.sample_inputs.args, **self.sample_inputs.kwargs
+            )
+            self._update_variables(variables)
+            self.sample_inputs = None
 
     def __call__(self, *args, **kwargs):
 
@@ -67,7 +73,7 @@ class FlaxModule(Module):
         if self.cache is not None:
             variables["cache"] = self.cache
 
-        rngs = self._get_rngs()
+        rngs = self._get_rngs(self.rngs)
 
         output, variables = self.module.value.apply(
             variables,
@@ -76,12 +82,12 @@ class FlaxModule(Module):
             rngs=rngs,
             **kwargs,
         )
-        self._process_variables(variables)
+        self._update_variables(variables)
 
         return output
 
-    def _get_rngs(self):
-        all_collections = tuple(set(self.rngs + self.mutable))
+    def _get_rngs(self, collections: tp.Sequence[str]):
+        all_collections = tuple(collections)
 
         if len(all_collections) == 0:
             rngs = {}
@@ -97,27 +103,21 @@ class FlaxModule(Module):
 
         return rngs
 
-    def _process_variables(self, variables: tp.Optional[FrozenDict]) -> None:
+    def _update_variables(self, variables: FrozenDict) -> None:
 
-        if variables is None:
-            self.params = None
-            self.batch_stats = None
-            self.cache = None
-            self.variables = None
-        else:
-            if "params" in variables:
-                params: FrozenDict
-                variables, params = variables.pop("params")
-                self.params = params.unfreeze()
+        if "params" in variables:
+            params: FrozenDict
+            variables, params = variables.pop("params")
+            self.params = params.unfreeze()
 
-            if "batch_stats" in variables:
-                batch_stats: FrozenDict
-                variables, batch_stats = variables.pop("batch_stats")
-                self.batch_stats = batch_stats.unfreeze()
+        if "batch_stats" in variables:
+            batch_stats: FrozenDict
+            variables, batch_stats = variables.pop("batch_stats")
+            self.batch_stats = batch_stats.unfreeze()
 
-            if "cache" in variables:
-                cache: FrozenDict
-                variables, cache = variables.pop("cache")
-                self.cache = cache.unfreeze()
+        if "cache" in variables:
+            cache: FrozenDict
+            variables, cache = variables.pop("cache")
+            self.cache = cache.unfreeze()
 
-            self.variables = variables.unfreeze()
+        self.variables = variables.unfreeze()
