12f1a290ad18333cf512652277f39af7dc19e5a6
Andres Velez <anvelezec@unal.edu.co>
2021-10-28 12:08:05 -0500

    mae metric and unit test

---
+++ tests/metrics/test_mean_absolute_error.py
@@ -0,0 +1,34 @@
+import jax.numpy as jnp
+import numpy as np
+
+import torch
+from torchmetrics import MeanAbsoluteError
+
+from treex.metrics.mean_absolute_error import MeanAbsolutError
+
+class TestMAE:
+    def test_mae_basic(self):
+        
+        y_true = np.random.randn(8, 20, 20)
+        y_pred = np.random.randn(8, 20, 20)
+
+        mean_absolute_error_instance = MeanAbsolutError()
+        mae_treex = mean_absolute_error_instance(**{"y_true":y_true, "y_pred":y_pred})
+
+        mean_absolute_error = MeanAbsoluteError()
+        mae_tm = mean_absolute_error(torch.from_numpy(y_pred), torch.from_numpy(y_true))
+        assert np.isclose(np.array(mae_treex), mae_tm.numpy())
+    
+
+    def test_accumulative_mae(self):
+        mean_absolute_error_instance = MeanAbsolutError()
+        mean_absolute_error = MeanAbsoluteError()
+        for batch in range(2):
+
+            y_true = np.random.randn(8, 5, 5)
+            y_pred = np.random.randn(8, 5, 5)
+            
+            mean_absolute_error_instance(**{"y_true":y_true, "y_pred":y_pred})
+            mean_absolute_error(torch.from_numpy(y_pred), torch.from_numpy(y_true))
+
+        assert np.isclose(np.array(mean_absolute_error_instance.compute()), mean_absolute_error.compute().numpy())
--- treex/metrics/mean_absolute_error.py
+++ treex/metrics/mean_absolute_error.py
@@ -6,37 +6,15 @@ from treex import types
 from treex.metrics.mean import Mean
 
 
-def _mean_absolute_error(y_pred: jnp.ndarray, y_true: jnp.ndarray):
-    """
-    Computes the mean absolute error between labels and predictions.
+def _mean_absolute_error(y_pred: jnp.ndarray, y_true: jnp.ndarray) -> jnp.ndarray:
+    """Calculates values required to update/compute Mean Absolute Error. Cast y_pred to have the same type as y_true.
 
-    After computing the absolute distance between the inputs, the mean value over
-    the last dimension is returned.
-
-    ```python
-    loss = mean(abs(y_true - y_pred), axis=-1)
-    ```
-
-    Usage:
-
-    ```python
-    rng = jax.random.PRNGKey(42)
-
-    y_pred = jax.random.uniform(rng, shape=(2, 3))
-    y_true = jax.random.randint(rng, shape=(2, 3), minval=0, maxval=2)
-
-    loss = elegy.losses.mean_absolute_error(y_true, y_pred)
-
-    assert loss.shape == (2,)
-    assert jnp.array_equal(loss, jnp.mean(jnp.abs(y_true - y_pred), axis=-1))
-
-    ```
-    Arguments:
-        y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
-        y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
+    Args:
+        y_pred: Predicted tensor 
+        y_true: Ground truth tensor 
 
     Returns:
-        Mean absolute error values. shape = `[batch_size, d0, .. dN-1]`.
+        jnp.ndarray values needed to update Mean Absolute Error 
     """
 
     y_true = y_true.astype(y_pred.dtype)
@@ -48,22 +26,37 @@ class MeanAbsolutError(Mean):
         self,
         on: tp.Optional[types.IndexLike] = None,
         name: tp.Optional[str] = None,
-        dtype: tp.Optional[jnp.dtype] = None,
-        **kwards
+        dtype: tp.Optional[jnp.dtype] = None
     ):
         """
-        Creates a `MeanAbsoluteError` instance.
+        `Computes Mean Absolute Error`_ (MAE):
+        .. math:: \text{MAE} = \frac{1}{N}\sum_i^N | y_i - \hat{y_i} |
+        Where :math:`y` is a tensor of target values, and :math:`\hat{y}` is a tensor of predictions.
 
-        Arguments:
-            on: A string or integer, or iterable of string or integers, that
+        Args:
+            on: 
+                A string or integer, or iterable of string or integers, that
                 indicate how to index/filter the `y_true` and `y_pred`
                 arguments before passing them to `call`. For example if `on = "a"` then
                 `y_true = y_true["a"]`. If `on` is an iterable
                 the structures will be indexed iteratively, for example if `on = ["a", 0, "b"]`
                 then `y_true = y_true["a"][0]["b"]`, same for `y_pred`. For more information
                 check out [Keras-like behavior](https://poets-ai.github.io/elegy/guides/modules-losses-metrics/#keras-like-behavior).
+            name:
+                Modules name
+            dtype: Metrics states initialization dtype
+                
+        
+        Example:
+        >>> import jax.numpy as jnp
+        >>> from treex.metrics.mean_absolute_error import MeanAbsolutError
+        
+        >>> y_true = jnp.array([3.0, -0.5, 2.0, 7.0])
+        >>> y_pred = jnp.array([3.0, -0.5, 2.0, 7.0])
 
-            kwargs: Additional keyword arguments passed to Module.
+        >>> mae = MeanAbsolutError()
+        >>> mae(y_pred, y_true)
+
         """
         super().__init__(on=on, name=name, dtype=dtype)
 
@@ -71,28 +64,21 @@ class MeanAbsolutError(Mean):
         self,
         y_true: jnp.ndarray,
         y_pred: jnp.ndarray,
-        sample_weight: tp.Optional[jnp.ndarray] = None,
     ) -> tp.Any:
         """
         Accumulates metric statistics. `y_true` and `y_pred` should have the same shape.
 
         Arguments:
-            y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
-            y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
-            sample_weight: Optional `sample_weight` acts as a
-                coefficient for the metric. If a scalar is provided, then the metric is
-                simply scaled by the given value. If `sample_weight` is a tensor of size
-                `[batch_size]`, then the metric for each sample of the batch is rescaled
-                by the corresponding element in the `sample_weight` vector. If the shape
-                of `sample_weight` is `[batch_size, d0, .. dN-1]` (or can be broadcasted
-                to this shape), then each metric element of `y_pred` is scaled by the
-                corresponding value of `sample_weight`. (Note on `dN-1`: all metric
-                functions reduce by 1 dimension, usually the last axis (-1)).
+            y_true: 
+                Ground truth values. shape = `[batch_size, d0, .. dN]`.
+            y_pred: 
+                The predicted values. shape = `[batch_size, d0, .. dN]`
         Returns:
             Array with the cumulative mean absolute error.
         """
-        values = _mean_absolute_error(self, y_pred, y_true, sample_weight)
-        return super().update(values, sample_weight)
+        values = _mean_absolute_error(y_pred, y_true)
+        return super().update(values)
 
     def compute(self):
-        return super().compute(self)
+        """Computes MAE based on inputs passed in to ``update`` previously."""
+        return super().compute()
