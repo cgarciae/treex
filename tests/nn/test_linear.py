2bfe52e7b16daa2b56cddebedcd5ca6c9a7c9a01
Cristian Garcia <cgarcia.e88@gmail.com>
2021-11-08 07:10:56 -0500

    Next key axis name (#25)
    * move Filter to types
    
    * update treeo
    
    * preserve_state

--- tests/nn/test_linear.py
+++ tests/nn/test_linear.py
@@ -1,7 +1,12 @@
+import functools
+import typing as tp
 import unittest
+from dataclasses import dataclass
 
+import einops
 import hypothesis as hp
 import jax
+import jax.numpy as jnp
 import numpy as np
 from flax import linen
 from hypothesis import strategies as st
@@ -22,6 +27,8 @@ KERNEL_INITS = BIAS_INITS + (
     tx.initializers.kaiming_uniform(),
     tx.initializers.kaiming_normal(),
 )
+
+A = tp.TypeVar("A")
 
 
 class LinearTest(unittest.TestCase):
@@ -128,3 +135,69 @@ class LinearTest(unittest.TestCase):
             np.allclose(a, b)
             for a, b in zip(jax.tree_leaves(module), jax.tree_leaves(module2))
         )
+
+    def test_axis_name(self):
+        class Linear3D(tx.Linear):
+            def __init__(
+                self,
+                num_heads: int,
+                *args,
+                axis_name: tp.Any = "batch",
+                **kwargs,
+            ):
+                super().__init__(*args, axis_name=axis_name, **kwargs)
+                self.num_heads = num_heads
+
+            def __call__(self, x):
+                def call(module: "Linear3D", x):
+                    x = super(module.__class__, module).__call__(x)
+                    return x, module
+
+                x = einops.repeat(x, "... -> head ...", head=self.num_heads)
+                x, module = jax.vmap(call, axis_name=self.axis_name)(self, x)
+
+                self.merge(module, inplace=True)
+
+                return x
+
+        num_heads = 4
+        units = 3
+
+        x = np.random.uniform(size=(5, 2))
+
+        module = Linear3D(num_heads, units).init(42, x)
+
+        y = module(x)
+
+        assert y.shape == (num_heads, 5, units)
+
+    def test_vmap_compact(self):
+        @dataclass
+        class Linear3D(tx.Module):
+            num_heads: int
+            units: int
+
+            @tx.preserve_state(jax.vmap, axis_name="head")
+            @tx.compact
+            def _forward(self, x: jnp.ndarray) -> jnp.ndarray:
+                x = tx.Linear(self.units, axis_name="head")(x)
+                return x
+
+            def __call__(self, x):
+                x = einops.repeat(x, "... -> head ...", head=self.num_heads)
+                x = self._forward(x)
+                return x
+
+        num_heads = 4
+        units = 3
+
+        x = np.random.uniform(size=(5, 2))
+
+        module = Linear3D(num_heads, units).init(42, x)
+
+        assert module.linear.kernel.shape == (num_heads, 2, units)
+        assert module.linear.bias.shape == (num_heads, units)
+
+        y = module(x)
+
+        assert y.shape == (num_heads, 5, 3)
