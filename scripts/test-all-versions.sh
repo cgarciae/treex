1403925ba29b73d8e0e581960083644effd9e9b9
Cristian Garcia <cgarcia.e88@gmail.com>
2021-11-08 14:14:35 -0500

    remove unused scripts

--- scripts/test-all-versions.sh
+++
@@ -1,7 +0,0 @@
-for python_version in "3.6" "3.7" "3.8"; do
-    if [[ $python_version =~ $kPYTHON_VERSIONS ]] || [[ -z "$python_version" ]]; then
-        bash scripts/test-version.sh "$python_version"
-    else
-        echo "Check python version"
-    fi
-done
