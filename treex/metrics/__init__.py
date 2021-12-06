2924906a74f680afb62894ee7323b85163481d98
Jose Miguel Arrieta <jmarrietar@gmail.com>
2021-12-06 08:37:27 -0500

    Add MSE and tests (#50)

--- treex/metrics/__init__.py
+++ treex/metrics/__init__.py
@@ -3,8 +3,10 @@ from .loss_and_logs import LossAndLogs
 from .losses import AuxLosses, Losses
 from .mean import Mean
 from .mean_absolute_error import MeanAbsoluteError
+from .mean_square_error import MeanSquareError
 from .metric import Metric
 from .metrics import AuxMetrics, Metrics
 from .reduce import Reduce, Reduction
 
 MAE = MeanAbsoluteError
+MSE = MeanSquareError
