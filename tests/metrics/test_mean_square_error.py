29d781ef86066ea8962e6504294cb4c602efa60f
Cristian Garcia <cgarcia.e88@gmail.com>
2022-01-10 12:17:17 -0500

    Fix MeanSquaredError and MeanAbsoluteError metrics argument names (#56)

--- tests/metrics/test_mean_square_error.py
+++ tests/metrics/test_mean_square_error.py
@@ -11,14 +11,14 @@ import treex as tx
 class TestMSE:
     def test_mse_basic(self):
 
-        y_true = np.random.randn(8, 20, 20)
-        y_pred = np.random.randn(8, 20, 20)
+        target = np.random.randn(8, 20, 20)
+        preds = np.random.randn(8, 20, 20)
 
         mse_tx = tx.metrics.MeanSquareError()
-        mse_tx_value = mse_tx(y_true=y_true, y_pred=y_pred)
+        mse_tx_value = mse_tx(target=target, preds=preds)
 
         mse_tm = tm.MeanSquaredError()
-        mse_tm_value = mse_tm(torch.from_numpy(y_pred), torch.from_numpy(y_true))
+        mse_tm_value = mse_tm(torch.from_numpy(preds), torch.from_numpy(target))
         assert np.isclose(np.array(mse_tx_value), mse_tm_value.numpy())
 
     def test_accumulative_mse(self):
@@ -26,11 +26,11 @@ class TestMSE:
         mse_tm = tm.MeanSquaredError()
 
         for batch in range(2):
-            y_true = np.random.randn(8, 5, 5)
-            y_pred = np.random.randn(8, 5, 5)
+            target = np.random.randn(8, 5, 5)
+            preds = np.random.randn(8, 5, 5)
 
-            mse_tx(y_true=y_true, y_pred=y_pred)
-            mse_tm(torch.from_numpy(y_pred), torch.from_numpy(y_true))
+            mse_tx(target=target, preds=preds)
+            mse_tm(torch.from_numpy(preds), torch.from_numpy(target))
 
         assert np.isclose(
             np.array(mse_tx.compute()),
@@ -39,11 +39,11 @@ class TestMSE:
 
     def test_mse_short(self):
 
-        y_true = np.random.randn(8, 20, 20)
-        y_pred = np.random.randn(8, 20, 20)
+        target = np.random.randn(8, 20, 20)
+        preds = np.random.randn(8, 20, 20)
 
-        mse_tx_long = tx.metrics.MeanSquareError()(y_true=y_true, y_pred=y_pred)
-        mse_tx_short = tx.metrics.MSE()(y_true=y_true, y_pred=y_pred)
+        mse_tx_long = tx.metrics.MeanSquareError()(target=target, preds=preds)
+        mse_tx_short = tx.metrics.MSE()(target=target, preds=preds)
         assert np.isclose(np.array(mse_tx_long), np.array(mse_tx_short))
 
     @hp.given(
@@ -52,10 +52,10 @@ class TestMSE:
     @hp.settings(deadline=None, max_examples=10)
     def test_mse_weights(self, use_sample_weight):
 
-        y_true = np.random.randn(8, 20, 20)
-        y_pred = np.random.randn(8, 20, 20)
+        target = np.random.randn(8, 20, 20)
+        preds = np.random.randn(8, 20, 20)
 
-        params = {"y_true": y_true, "y_pred": y_pred}
+        params = {"target": target, "preds": preds}
         mse_tx = tx.metrics.MeanSquareError()
 
         if use_sample_weight:
@@ -67,7 +67,7 @@ class TestMSE:
         mse_tx_value = mse_tx(**params)
 
         if use_sample_weight:
-            y_true, y_pred = y_true[sample_weight == 1], y_pred[sample_weight == 1]
+            target, preds = target[sample_weight == 1], preds[sample_weight == 1]
 
         mse_tx = tx.metrics.MeanSquareError()
         mse_tx_no_sample_weight = mse_tx(**params)
@@ -80,10 +80,10 @@ class TestMSE:
     @hp.settings(deadline=None, max_examples=10)
     def test_mse_weights_values_dim(self, use_sample_weight):
 
-        y_true = np.random.randn(8, 20, 20)
-        y_pred = np.random.randn(8, 20, 20)
+        target = np.random.randn(8, 20, 20)
+        preds = np.random.randn(8, 20, 20)
 
-        params = {"y_true": y_true, "y_pred": y_pred}
+        params = {"target": target, "preds": preds}
         if use_sample_weight:
             sample_weight = np.random.choice([0, 1], 8 * 20).reshape((8, 20))
             params.update({"sample_weight": sample_weight})
